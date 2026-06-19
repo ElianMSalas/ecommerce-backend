5883480bf517fa86881bf05c08c80275c883c135
elianmsalas <emunozsalas@icloud.com>
2026-06-19 00:21:41 -0400

    chore: estructura inicial del proyecto

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const app = require("./app");
+const pool = require("./config/db");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto: ${PORT}`);
+});
